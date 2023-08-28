ad5fa0c1900000862f53ee66e3d0319d5aaf81d2
umumumumum1230987765 <97182506+umumumumum1230987765@users.noreply.github.com>
2023-08-28 22:38:08 +0530

    Update apiKeys.js

--- apiKeys.js
+++ apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
+  key: "7c87db507b9698af0a03b0de28aee590",
   base: "https://api.openweathermap.org/data/2.5/",
 };
